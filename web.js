743b5156e0597b27086305c8cad5e341a2b0c27b
jason winkel <jason.winkel@greenheck.com>
2013-07-08 20:17:33 +0000

    test2

--- web.js
+++ web.js
@@ -1,7 +1,17 @@
 var express = require('express');
+
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response){ var fs = require('fs'); var buffer = new buffer (); response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))});
- });
+app.get('/', function(request, response) {
+    var fs = require('fs');
 
-var port = process.env.PORT || 5000;app.listen(port, function() {console.log("Listening on " + port);});
+    var buf = new Buffer (fs.readFileSync("index.html", "utf-8"));
+
+    response.send(buf.toString());
+});
+
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
